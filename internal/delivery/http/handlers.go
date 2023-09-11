cd1a60da34035ef19ebd7bae247302505d02b388
Jhnvlglmlbrt <user.mail0encrypt@gmail.com>
2023-09-11 20:36:49 +0300

    del unused struct

--- internal/delivery/http/handlers.go
+++ internal/delivery/http/handlers.go
@@ -1,15 +1,10 @@
 package handler
 
 import (
-	"basic-auth/internal/entity"
 	"fmt"
 	"log"
 	"net/http"
 )
-
-type User struct {
-	entity.User
-}
 
 func ProtectedHandler(w http.ResponseWriter, r *http.Request) {
 	_, err := fmt.Fprintln(w, "This is the protected handler")
